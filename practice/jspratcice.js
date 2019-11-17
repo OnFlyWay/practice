d9e5e9f6933605a9d17bbb28641fcfc8d373149a
xixi <823123616@qq.com>
2019-11-17 14:06:49 +0800

    add practice

---
+++ practice/jspratcice.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @LastEditors: Please set LastEditors
+ * @Date: 2019-04-06 11:01:56
+ * @LastEditTime: 2019-09-10 22:46:29
+ */
+let name=function(){
+    console.log("hello world")
+}
+name.sayMyself=[1,2,3]
+console.log(name.sayMyself)
